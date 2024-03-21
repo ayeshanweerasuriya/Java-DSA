133a9653ac7c53672213e416e10beb1223f087d6
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2024-03-21 23:05:53 +0530

    init

--- Sample.java
+++ Sample.java
@@ -4,6 +4,17 @@ public class Sample {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("\u0041");
-    }
+        System.out.print("Type the amount of bytes: ");
+        int bytes = scan.nextInt();
+        scan.close();
+
+        double kilobytes = bytes / 1024.0;
+        double megabytes = kilobytes / 1024.0;
+        double gigabytes = megabytes / 1024.0;
+
+        System.out.println("\nSize in Kilobytes (KB): " + kilobytes + " kb");
+        System.out.println("Size in Megabytes (MB): " + megabytes + " mb");
+        System.out.println("Size in Gigabytes (GB): " + gigabytes + " gb");
+
+        }
 }
