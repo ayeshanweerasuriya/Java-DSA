b1d02d72eb1fcaaf31573f3291e313cf110225ac
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2024-03-24 08:36:24 +0530

    file name change

---
+++ Example.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Example{
+	public static void main(String args[]){
+		Scanner input=new Scanner(System.in);
+		System.out.print("Input an integer : ");
+		int num=input.nextInt();
+		System.out.println("A");
+		System.out.println("B");
+		if (num > 100) System.out.println("C");
+		System.out.println("D");
+		System.out.println("E");
+	}
+}
