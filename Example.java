498eed7ccfda00766a76a2c562039b7a3ba733ef
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2024-03-28 21:00:29 +0530

    find the time complexity of loops

--- Example.java
+++ Example.java
@@ -1,24 +1,41 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 class Example {
     public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.print("Enter the weight of the parcel (kg): ");
-        double weight = scan.nextDouble();
-        scan.close();
+        // System.out.print("Input a String: ");
+        // String sentence = scan.nextLine();
+        // scan.close();
 
-        double courier_charge = 0;
+        ArrayList<String> words = new ArrayList<>(Arrays.asList("James", "Mason", "Lucky"));
 
-        if (weight <= 5) {
-            courier_charge = 500;
-            
-        } else {
-            double extra_Kg = weight - 5;
-            courier_charge = 500 + extra_Kg * 100;
+        String[] numbers = new String[3];
+
+        // String[] word2 = { "Ayeshan", "James", "Mason" };
+
+        // Measure time for enhanced for loop
+        long startTime1 = System.nanoTime();
+
+        for (String word : words) {
+            System.out.println(word);
         }
 
-        System.out.println("\nWeight of the parcel: " + weight + "kg");
-        System.out.printf("Courier charge for the parcel: Rs.%.2f", courier_charge);
+        long endTime1 = System.nanoTime();
+        long duration1 = endTime1 - startTime1;
+        System.out.println("Duration of enhanced for loop: " + duration1 + " nanoseconds");
+
+        // Measure time for standard for loop
+        long startTime2 = System.nanoTime();
+
+        for (int i = 0; i < words.size(); i++) {
+            System.out.println(words.get(i));
+        }
+
+        long endTime2 = System.nanoTime();
+        long duration2 = endTime2 - startTime2;
+        System.out.println("Duration of standard for loop: " + duration2 + " nanoseconds");
     }
 }
