4f2d88bb1fa13e41e3b0cb46a46bdeabc53de55f
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2024-04-03 20:58:33 +0530

    note counter

--- Example.java
+++ Example.java
@@ -1,21 +1,11 @@
-import java.util.*;
-
 class Example {
     public static void main(String args[]) {
-        Scanner input = new Scanner(System.in);
 
-        System.out.print("Input an integer : ");
-        int num = input.nextInt();
-
-        // Find no of digits
-        int count = 0;
-
-        do {
-            num = num / 10; // num/=10;
-            count++;
-        } while (num != 0);
-
-        System.out.println("No of digits : " + count);
+        {
+            int i = 0;
+        }
+        for (int i = 0; i < 10; i++) {
+        }
 
     }
 }
