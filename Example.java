26b932e5ac8d46a140ae7503d8b861e3fcaa457a
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2024-04-07 22:34:20 +0530

    guess the output

--- Example.java
+++ Example.java
@@ -1,31 +1,10 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-
-public class Example {
+class Example {
     public static void main(String[] args) {
-
-        String[] words = {};
-
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader("D:\\ICET\\ICET.txt"));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                words = line.split(" ");
-
-                for (String word : words) {
-
-                    if (word.equalsIgnoreCase("vs")) {
-                        continue;
-                    } else {
-                        System.out.println(word);
-                    }
-                }
-            }
-            reader.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        int x = 0;
+        // Line 4 --insert code here--
+        int y = 11;
+        do {
+        } while (x++ < y);
+        System.out.println(x);
     }
 }
