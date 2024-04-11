7c8a14f2af0c929d3820599f55166260b4cfe76f
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2024-04-11 11:01:21 +0530

    convertion

--- Example.java
+++ Example.java
@@ -1,14 +1,10 @@
 
-// What will be the output?
 class Example {
-    public static void main(String args[]) {
-        int x = 1;
-        L1: if (x > 0) {
-            System.out.print("A");
-            L2: if (x == 1) {
-                break L1;
-            }
-            System.out.print("B");
-        }
+    public static void main(String s[]) {
+
+        int tot = 971;
+        double avg;
+        avg = tot / (double) 10;
+        System.out.println("Average: " + avg);
     }
 }
