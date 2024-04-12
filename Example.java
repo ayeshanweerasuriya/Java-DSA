185fe3e9bb1d4319505ff906b09a0871bef04e19
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2024-04-12 16:48:49 +0530

    Calculate Monthly Interest

--- Example.java
+++ Example.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
 
 class Example {
-    public static void main(String s[]) {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
-        int tot = 971;
-        double avg;
-        avg = tot / (double) 10;
-        System.out.println("Average: " + avg);
+        System.out.print("Input monthly interest: ");
+        double monthlyInterestRate = scan.nextDouble();
+        scan.close();
+
+        double depositAmount = calDeposit(monthlyInterestRate);
+        System.out.println("\nYou should deposit: Rs." + depositAmount);
+    }
+
+    public static double calDeposit(double monthlyInterestRate) {
+        double monthlyRate = 0.20 / 12.0;
+        double deposit = monthlyInterestRate / (Math.pow(1 + monthlyRate, 12) - 1);
+        return deposit;
     }
 }
