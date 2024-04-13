00644329ced95e521ebf34d38eed9991baf859b0
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2024-04-13 17:58:08 +0530

    init star pattern

--- Example.java
+++ Example.java
@@ -1,20 +1,7 @@
-import java.util.Scanner;
-
 class Example {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-
-        System.out.print("Input monthly interest: ");
-        double monthlyInterestRate = scan.nextDouble();
-        scan.close();
-
-        double depositAmount = calDeposit(monthlyInterestRate);
-        System.out.println("\nYou should deposit: Rs." + depositAmount);
-    }
-
-    public static double calDeposit(double monthlyInterestRate) {
-        double monthlyRate = 0.20 / 12.0;
-        double deposit = monthlyInterestRate / (Math.pow(1 + monthlyRate, 12) - 1);
-        return deposit;
+        for (int i = 0; i < 10; i++) {
+            System.out.println("*");
+        }
     }
 }
