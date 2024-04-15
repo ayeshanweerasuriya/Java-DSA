17299d7bed2d18bb2f71e875ddfa2b5d4f0277e8
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2024-04-15 23:16:56 +0530

    Program to find the DividedPoint

--- Example.java
+++ Example.java
@@ -1,23 +1,33 @@
-class Demo {
-    public static void test() {
-        System.out.println("Hello....");
+import java.util.Scanner;
+
+public class Example {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the coordinates of the first point (xA)    : ");
+        double xA = scanner.nextDouble();
+        System.out.print("Enter the coordinates of the first point (yA)    : ");
+        double yA = scanner.nextDouble();
+
+        System.out.print("Enter the coordinates of the second point (xB)   : ");
+        double xB = scanner.nextDouble();
+        System.out.print("Enter the coordinates of the second point (yB)   : ");
+        double yB = scanner.nextDouble();
+
+        System.out.print("Enter the ratio (xC) in which the line is divided: ");
+        double xC = scanner.nextDouble();
+        System.out.print("Enter the ratio (yC) in which the line is divided: ");
+        double yC = scanner.nextDouble();
+
+        calculateDividedPoint(xA, yA, xB, yB, xC, yC);
+
+        scanner.close();
     }
 
-    public static void test(String name) {
-        System.out.println("Hello " + name + "....");
-    }
+    public static void calculateDividedPoint(double xA, double yA, double xB, double yB, double xC, double yC) {
+        double x = (xC * xB + yC * xA) / (xC + yC);
+        double y = (xC * yB + yC * yA) / (xC + yC);
 
-    public static void main(String args[]) {
-        String s = "java"; // Line 1
-        // test; //Line 2
-        test(); // Line 3
-        // test(){ } //Line 4
-        // test(){ }; //Line 5
-        Demo.test(); // Line 6
-        System.out.println("test()"); // Line 7
-        // System.out.println(test()); //Line 8
-        test(s); // Line 9
-        // System.out.println(test(s)); //Line 10
-        System.out.println("test(s)"); // Line 11
+        System.out.print("\nCoordinates of the point where the line divides  : (" + x + ", " + y + ")");
     }
 }
