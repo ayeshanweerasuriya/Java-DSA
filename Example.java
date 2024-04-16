90ed098fbac8d1f17f3394628c705409e1efed99
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2024-04-16 13:30:07 +0530

    init Me program

--- Example.java
+++ Example.java
@@ -1,25 +1,3 @@
-import java.util.Scanner;
-
 public class Example {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
 
-        System.out.print("Enter the first integer number: ");
-        int intNum1 = scan.nextInt();
-        System.out.print("Enter the second integer number: ");
-        int intNum2 = scan.nextInt();
-
-        int intDiff = absoluteDifference(intNum1, intNum2);
-        System.out.println("Absolute difference of two integers: " + intDiff);
-
-        scan.close();
-    }
-
-    public static int absoluteDifference(int num1, int num2) {
-        return Math.abs(num1 - num2);
-    }
-
-    public static double absoluteDifference(double num1, double num2) {
-        return Math.abs(num1 - num2);
-    }
 }
---
+++ Me.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Me {
+    private static final String password = "Ayeshan20";
+    private static int passwordAttempts = 0;
+
+    public static void checkPassword() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter password: ");
+        String userInput = scanner.nextLine();
+
+        if (userInput.equals(password)) {
+            displayDetails();
+        } else {
+            passwordAttempts++;
+            if (passwordAttempts < 3) {
+                System.out.print("Incorrect password. Do you want to continue (Yes/No)? ");
+                String choice = scanner.nextLine();
+                if (choice.equalsIgnoreCase("Yes")) {
+                    checkPassword();
+                } else {
+                    System.out.println("Program ended.");
+                }
+            } else {
+                System.out.println("You've exceeded the maximum number of password attempts. Program ended.");
+            }
+        }
+        scanner.close();
+    }
+
+    public static void displayDetails() {
+        System.out.println("\n------Welcome! Here are my details------\n");
+        System.out.println("Name: Ayeshan Weerasuriya");
+        System.out.println("Age: 23");
+    }
+
+    public static void main(String[] args) {
+        checkPassword();
+    }
+}
