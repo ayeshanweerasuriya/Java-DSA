2dfbbe69ecaaa3754f3b2dad101bbcecdc8012a3
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2024-04-21 09:12:44 +0530

    init array

--- Example.java
+++ Example.java
@@ -1,3 +1,10 @@
-public class Example {
+import java.util.*;
 
+class Example {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+
+        int[] averageMarks = new int[11];
+
+    }
 }
