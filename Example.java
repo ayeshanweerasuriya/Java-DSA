0bc62bb5d3c0d88408432ac549bba28159edc924
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2024-04-21 09:24:36 +0530

    init array for find min and total

--- Example.java
+++ Example.java
@@ -3,8 +3,29 @@ import java.util.*;
 class Example {
     public static void main(String args[]) {
         Scanner input = new Scanner(System.in);
+        int[] ar = new int[10];
 
-        int[] averageMarks = new int[11];
+        // ---------------------Input------------------------
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Input number " + (i + 1) + " : ");
+            ar[i] = input.nextInt();
+        }
 
+        // ----------------------find total----------------------
+        int total = 0;
+        for (int i = 0; i < 10; i++) {
+            total += ar[i];
+        }
+
+        // ----------------------find maximum-------------------
+        int min = ar[0];
+        for (int i = 0; i < 10; i++) {
+            if (min > ar[i]) {
+                min = ar[i];
+            }
+        }
+
+        System.out.println("Total   : " + total);
+        System.out.println("Maximum : " + min);
     }
 }
