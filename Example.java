9ce4fccba4e2408bcf1772f225b3151190cb5bd5
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2024-04-23 18:59:41 +0530

    int Example

--- Example.java
+++ Example.java
@@ -1,22 +1,7 @@
 import java.util.*;
 
 class Example {
-
-    public static void main(String args[]) {
-        int[] arr = new int[0];
-        arr[0] = 12;
-        arr[1] = 35;
-        arr[2] = 47;
-        arr[3] = 59;
-        arr[4] = 62;
-        arr[5] = 73;
-        arr[6] = 81;
-        arr[7] = 93;
-        arr[8] = 111;
-        arr[9] = 121;
-
-        System.out.println(arr[0]);
-        System.out.println(arr[3]);
-        System.out.println(arr[5]);
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
     }
 }
