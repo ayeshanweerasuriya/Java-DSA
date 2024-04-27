ce5670f3f5b88b6dee0d76eeb07f0fff4bdef87e
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2024-04-28 00:29:40 +0530

    init

--- Example.java
+++ Example.java
@@ -1,13 +1,10 @@
-import java.util.*;
-
 class Example {
     public static void main(String args[]) {
-        int[] ar = { 40, 20, 10, 50, 30, 40, 60, 20, 10, 90, 40, 30 };
-        System.out.println(Arrays.toString(ar));// [40,20,10,50,30,40,60,20,10,90,40,30]
-
-        // Remove duplicate elements
-        //
-        //
-        System.out.println(Arrays.toString(ar));// [40,20,10,50,30,60,90]
+        int x = 0;
+        int[] xr = new int[3];
+        double d = 0.0;
+        double[] dr = new double[5];
+        int[] grade = { 'a', 'b' };
+        // Insert code here //Line 20
     }
 }
