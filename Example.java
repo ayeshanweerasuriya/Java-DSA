6a2b17b743854921d7dc995c31acd7698331c94e
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2024-04-28 12:07:03 +0530

    init table incomplete

--- Example.java
+++ Example.java
@@ -2,25 +2,37 @@ import java.util.*;
 
 class Example {
     public static void main(String args[]) {
-        int[][] marks = new int[10][4];// 10-->No of students,4-->No of subjects
 
         Scanner input = new Scanner(System.in);
-        Random random = new Random();
+        System.out.print("Input no of students : ");
+        final int N = input.nextInt();
 
-        for (int i = 0; i < 10; i++) {
-            // System.out.println("Input marks for students " + (i + 1) + "....");
-            for (int j = 0; j < 4; j++) {
-                // System.out.print("\tSubject " + (j + 1) + " : ");
-                int randomNumber = random.nextInt(100);
-                marks[i][j] = randomNumber;
+        System.out.print("Input no of subjects : ");
+        final int S = input.nextInt();
+
+        int[][] marks = new int[N][S];
+        int[] stTot = new int[N];
+
+        Random r = new Random();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < S; j++) {
+                marks[i][j] = r.nextInt(101);
             }
         }
 
-        for (int i = 0; i < marks.length - 1; i++) {
-            for (int j = 0; j < marks[i].length; j++) {
-                System.out.print(marks[i][j] + " ");
+        // Find student total marks
+
+        System.out.print("StNo\t");
+        for (int j = 0; j < S; j++) {
+            System.out.print("Sub" + (j + 1) + "\t");
+        }
+        System.out.println("Tot\tMax\tMin\tAvg\tGrade\tRank");
+        for (int i = 0; i < N; i++) {
+            System.out.print(1000 + i + "\t");
+            for (int j = 0; j < S; j++) {
+                System.out.print(marks[i][j] + "\t");
             }
-            System.out.println();
+            System.out.println(stTot[i]);
         }
     }
 }
