a1d0cda4e4394d01617d6eaf9725f85008451fbd
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2024-04-29 12:28:19 +0530

    fully completed contactOrganizer

--- Example.java
+++ Example.java
@@ -1,38 +1,29 @@
-import java.util.*;
+import java.util.Arrays;
 
-class Example {
-    public static void main(String args[]) {
+public class Example {
+    public static void main(String[] args) {
+        String[] subjects = { "Mathematics", "Science", "English", "Sinhala", "History", "Buddhism", "Business Studies",
+                "Computer Studies" };
 
-        Scanner input = new Scanner(System.in);
-        System.out.print("Input no of students : ");
-        final int N = input.nextInt();
+        subjects = addSubject(subjects, "Music");
 
-        System.out.print("Input no of subjects : ");
-        final int S = input.nextInt();
+        System.out.println("Updated Subject List: " + Arrays.toString(subjects));
+    }
 
-        int[][] marks = new int[N][S];
-        int[] stTot = new int[N];
+    public static String[] addSubject(String[] subjects, String newSubject) {
+        String[] updatedSubjects = new String[subjects.length + 1];
+        int index = 7;
 
-        Random r = new Random();
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < S; j++) {
-                marks[i][j] = r.nextInt(101);
-            }
+        for (int i = 0; i < index; i++) {
+            updatedSubjects[i] = subjects[i];
         }
 
-        // Find student total marks
+        updatedSubjects[index] = newSubject;
 
-        System.out.print("StNo\t");
-        for (int j = 0; j < S; j++) {
-            System.out.print("Sub" + (j + 1) + "\t");
+        for (int i = index + 1; i < updatedSubjects.length; i++) {
+            updatedSubjects[i] = subjects[i - 1];
         }
-        System.out.println("Tot\tMax\tMin\tAvg\tGrade\tRank");
-        for (int i = 0; i < N; i++) {
-            System.out.print(1000 + i + "\t");
-            for (int j = 0; j < S; j++) {
-                System.out.print(marks[i][j] + "\t");
-            }
-            System.out.println(stTot[i]);
-        }
+
+        return updatedSubjects;
     }
 }
