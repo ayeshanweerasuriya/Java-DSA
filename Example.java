550b03b3d271e1fb9b1c3fcbee468a5b28b21263
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2024-05-10 21:35:28 +0530

    jump search without sorting

--- Example.java
+++ Example.java
@@ -1,10 +1,10 @@
-import java.time.LocalTime;
-import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Example {
     public static void main(String[] args) {
         Random random = new Random(100);
+        Scanner scan = new Scanner(System.in);
 
         int[] array = new int[100];
 
@@ -12,48 +12,42 @@ public class Example {
             array[i] = random.nextInt(100);
         }
 
-        // Sort the array
-        Arrays.sort(array);
+        int i = 0;
+        for (int num : array) {
+            System.out.println("index " + i + "\t" + num);
+            i++;
+        }
 
-        // int i = 0;
-        // for (int num : array) {
-        // LocalTime currentTime = LocalTime.now();
-        // System.out.println("Current time: " + currentTime);
+        System.out.print("Input the Target Value: ");
+        int targetValue = scan.nextInt();
+        scan.close();
 
-        // System.out.print(i + ". :\t");
-        // System.out.println(num);
-        // i++;
-        // }
-
-        jumpSearch(array, 50);
+        jumpSearch(array, targetValue);
     }
 
-    public static void jumpSearch(int[] arr, int targetValue) {
-        int arrayLength = arr.length;
-        double jump = Math.sqrt(arrayLength);
-        int jumpSize = (int) jump;
+    public static void jumpSearch(int[] array, int targetValue) {
+        int arrayLength = array.length;
+        double squreRoot = Math.sqrt(arrayLength);
+        int n = (int) squreRoot;
 
-        LocalTime startTime = LocalTime.now();
-        System.out.println("Jump search started at: " + startTime);
-
-        for (int i = 0; i < arrayLength; i += jumpSize) {
-            if (arr[i] == targetValue) {
-                System.out.println("Using Jump search: " + i);
-                LocalTime endTime = LocalTime.now();
-                System.out.println("Jump search ended at: " + endTime);
-                return;
-            } else if (arr[i] > targetValue) {
-                for (int j = i - jumpSize; j < i; j++) {
-                    if (j >= 0 && arr[j] == targetValue) {
-                        System.out.println("Using Jump search: " + j);
-                        LocalTime endTime = LocalTime.now();
-                        System.out.println("Jump search ended at: " + endTime);
-                        return;
-                    }
-                }
+        System.out.println("\n---------------------------------------------\n");
+        System.out.println("Array Length: " + arrayLength);
+        for (int i = 0; i < array.length; i += n) {
+            if (array[i] == targetValue) {
+                System.out.println("Target value at index: " + i);
                 break;
             }
+
+            if (array[i] > targetValue) {
+                for (int j = i; i >= n; j--) {
+                    if (array[j] == targetValue) {
+                        System.out.println("Target value at index: " + i);
+                        break;
+                    }
+                }
+            }
         }
-        System.out.println("Target value not found using Jump search.");
+
+        System.out.println("Target value is not in the array.");
     }
 }
