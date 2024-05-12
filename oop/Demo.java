2aad7cd8c74b78654a6f632519a5c0d70666c14e
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2024-05-12 16:42:41 +0530

    init bubble sort

--- oop/Demo.java
+++ oop/Demo.java
@@ -1,32 +1,54 @@
 package oop;
 
-class Box {
-    int length = 12;
-    int width = 5;
-    int height = 3;
+class Order {
+    String id; // customer id
+    double amount;
 
-    Box() {
-        length = 1;
-        width = 2;
-        height = 3;
-    }
-
-    Box(int length, int width, int height) {
-
+    Order(String id, double amount) {
+        this.id = id;
+        this.amount = amount;
     }
 }
 
 class Demo {
-    public static void main(String args[]) {
-        Box b1 = new Box();
-        System.out.println("length : " + b1.length); // 1
-        System.out.println("width  : " + b1.width); // 2
-        System.out.println("height : " + b1.height); // 3
-        System.out.println();
+    public static void printOrders(Order[] orderArray) {
+        System.out.print("[");
+        for (int i = 0; i < orderArray.length; i++) {
+            System.out.print(orderArray[i].id + "-" + orderArray[i].amount + ", ");
+        }
+        System.out.println("\b\b]");
+    }
 
-        Box b2 = new Box(0, 0, 0);
-        System.out.println("length : " + b2.length); //
-        System.out.println("width  : " + b2.width); //
-        System.out.println("height : " + b2.height); //
+    public static void sortOrderArray(Order[] orderArray) {
+
+        for (int i = 0; i < orderArray.length - 1; i++) {
+            for (int j = 0; j < orderArray.length - i - 1; j++) {
+                if (orderArray[j].amount > orderArray[j + 1].amount) {
+                    double temp = orderArray[j].amount;
+                    orderArray[j].amount = orderArray[j + 1].amount;
+                    orderArray[j + 1].amount = temp;
+
+                    String tempId = orderArray[j].id;
+                    orderArray[j].id = orderArray[j + 1].id;
+                    orderArray[j + 1].id = tempId;
+                }
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        Order[] orderArray = {
+                new Order("C001", 4500),
+                new Order("C002", 2500),
+                new Order("C003", 6500),
+                new Order("C004", 1500),
+                new Order("C005", 3500),
+                new Order("C005", 5500),
+        };
+        printOrders(orderArray);// [C001-4500, C002-2500, C003-6500,C004-1500,C005-3500,C005-5500]
+
+        sortOrderArray(orderArray);
+
+        printOrders(orderArray);// [C001-1500,C002-2500,C005-3500,C001-4500,C005-5500,C003-6500]
     }
 }
