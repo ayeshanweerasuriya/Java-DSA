be230abfffeafd63a4d6970e49489a99e443617f
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2024-05-14 23:36:08 +0530

    create ractangle class

--- oop/Demo.java
+++ oop/Demo.java
@@ -1,21 +1,43 @@
-class MyClass {
-    /* * Insert Code Here Line 10 */
-    int x;
-    int y;
+class Rectangle {
+    private float length = 1;
+    private float width = 1;
 
-    MyClass(int i, int j) {
-        x = 100;
-        y = 200;
+    public float getPerimeter() {
+        return 2 * (this.length + this.width);
     }
 
-    void printValues() {
-        System.out.println("Values : " + x + ", " + y);
+    public float getArea() {
+        return this.length * this.width;
+    }
+
+    public float getLength() {
+        return this.length;
+    }
+
+    public float getWidth() {
+        return this.width;
+    }
+
+    public void setLength(float length) {
+        if (length > 0.0 && length < 20.0) {
+            this.length = length;
+        } else {
+            System.out.println("Invalid Input");
+        }
+    }
+
+    public void setWidth(float width) {
+        if (length > 0.0 && length < 20.0) {
+            this.width = width;
+        } else {
+            System.out.println("Invalid Input");
+        }
     }
 }
 
 class Demo {
-    public static void main(String[] args) {
-        MyClass c = new MyClass(100, 200);
-        c.printValues();
+    public static void main(String args[]) {
+        Rectangle r1 = new Rectangle();
+        Rectangle r2 = new Rectangle();
     }
 }
