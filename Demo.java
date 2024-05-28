e581d6b83c8d54fbff0455bc03d88cb54214aee0
ayeshanweerasuriya <ayeshanweerasuriya@gmail.com>
2024-05-28 22:45:16 +0530

    queue

--- Demo.java
+++ Demo.java
@@ -1,40 +1,111 @@
-class Stack {
-    Node top;
+import java.util.*;
 
-    public void push(int data) {
+class Queue {
+    private Node front;
+
+    public void enQueue(int data) {
         Node n1 = new Node(data);
-        n1.next = top;
-        top = n1;
-    }
-
-    public void printStack() {
-        System.out.print("[");
-        Node current = top;
-        while (current != null) {
-            System.out.print(current.data + ", ");
-            current = current.next;
+        if (front == null) {
+            front = n1;
+        } else {
+            Node temp = front;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = n1;
         }
-        System.out.print("\b\b]");
     }
-}
 
-class Node {
-    int data;
-    Node next;
+    public boolean isEmpty() {
+        return front == null;
+    }
 
-    Node(int data) {
-        this.data = data;
+    public int size() {
+        Node temp = front;
+        int count = 0;
+        while (temp != null) {
+            temp = temp.next;
+            count++;
+        }
+        return count;
+    }
+
+    public void printQueue() {
+        System.out.print("[");
+        Node temp = front;
+        while (temp != null) {
+            System.out.print(temp.data + ", ");
+            temp = temp.next;
+        }
+        System.out.println(isEmpty() ? "empty]" : "\b\b]");
+    }
+
+    public void deQueue() {
+        front = front.next;
+    }
+
+    public void clear() {
+        front = null;
+    }
+
+    public int peek() {
+        return isEmpty() ? -1 : front.data;
+    }
+
+    public int search(int data) {
+        Node temp = front;
+        int index = -1;
+        while (temp != null) {
+            index++;
+            if (temp.data == data) {
+                return index;
+            }
+            temp = temp.next;
+        }
+        return index;
+    }
+
+    public int[] toArray() {
+        int[] tempDataArray = new int[size()];
+        Node temp = front;
+        for (int i = 0; i < tempDataArray.length; i++) {
+            tempDataArray[i] = temp.data;
+            temp = temp.next;
+        }
+        return tempDataArray;
+    }
+
+    public int poll() {
+        if (isEmpty()) {
+            return -1;
+        }
+        int data = front.data;
+        deQueue();
+        return data;
+    }
+
+    class Node {
+        private int data;
+        private Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
     }
 }
 
 class Demo {
     public static void main(String args[]) {
-        Stack s1 = new Stack();
-        s1.push(10);
-        s1.push(20);
-        s1.push(30);
-        s1.push(40);
-        s1.printStack();
-        s1.printStack();
+        Queue q1 = new Queue();
+        q1.enQueue(10);
+        q1.enQueue(20);
+        q1.enQueue(30);
+        q1.enQueue(40);
+        q1.enQueue(50);
+        q1.printQueue();// [10,20,30,40,50]
+
+        q1.deQueue();
+        q1.printQueue();// [20,30,40,50]
+
     }
 }
